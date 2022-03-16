aaa6ce9a97f83adc1b915c828cd0e5ebb3eb3b96
SenruoYusei <senruo0827@gmail.com>
2022-03-17 06:51:17 +0900

    update

--- WebApplication/shiftCollection/src/main/java/model/MemberSet.java
+++ WebApplication/shiftCollection/src/main/java/model/MemberSet.java
@@ -8,7 +8,12 @@ public class MemberSet extends ArrayList<Member>{
 	}
 	public String[] getSubmittedSchedule(int m, int t) {
 		schedule = new String[16];
-		for(int i = 0;i < 30;i++) {
+		Member firstMember = get(0);
+		String[] data = firstMember.getAllSchedule();
+		for(int i = 0;i < data.length;i++){
+			schedule[i] = firstMember.getName() + "," + data[i] + "\n";
+		}
+		for(int i = 1;i < 30;i++) {
 			Member member = (i < size()) ? get(i) : null;
 			
 			String[] submittedData = (member == null || member.getMonth() != m || member.getTermNum() != 15 * t) ? new String[16] : member.getAllSchedule();
