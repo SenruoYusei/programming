85ef1d519653575b67cfb5af1ce8c215e36d429e
SenruoYusei <senruo0827@gmail.com>
2022-03-16 13:55:58 +0900

    update

--- WebApplication/shiftCollection/src/main/java/servlet/GenerateCsv.java
+++ WebApplication/shiftCollection/src/main/java/servlet/GenerateCsv.java
@@ -31,8 +31,9 @@ public class GenerateCsv extends HttpServlet {
 		String filename = "output.csv";
 		//GenerateCSV gc = new GenerateCSV(output, filename);
 		
-
-		response.setHeader("Context-Type", "text/csv; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		//response.setHeader("Context-Type", "text/csv; charset=UTF-8");
 		response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
 		PrintWriter out = response.getWriter();
 		for(int i = 0;i < output.length;i++) {
