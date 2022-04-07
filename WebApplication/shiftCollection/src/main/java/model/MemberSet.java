30b23a92a6f4e95697d2c87a8921b309f9b455bf
SenruoYusei <senruo0827@gmail.com>
2022-04-08 06:40:12 +0900

    update
    update viewShift

--- WebApplication/shiftCollection/src/main/java/model/MemberSet.java
+++ WebApplication/shiftCollection/src/main/java/model/MemberSet.java
@@ -11,14 +11,14 @@ public class MemberSet extends ArrayList<Member>{
 		Member firstMember = get(0);
 		String[] data = firstMember.getAllSchedule();
 		for(int i = 0;i < data.length;i++){
-			schedule[i] = firstMember.getName() + "," + data[i] + "\n";
+			schedule[i] = firstMember.getName() + "," + (( firstMember.getMonth() != m || firstMember.getTerm() != t) ? "" : data[i]) + "\n";
 		}
 		for(int i = 1;i < 30;i++) {
 			Member member = (i < size()) ? get(i) : null;
 			
-			String[] submittedData = (member == null || member.getMonth() != m || member.getTerm() != 15 * t) ? new String[16] : member.getAllSchedule();
+			String[] submittedData = (member == null || member.getMonth() != m || member.getTerm() != t) ? new String[16] : member.getAllSchedule();
 			for(int j = 0;j < submittedData.length;j++) {
-				schedule[j] += (member == null ? "" : member.getName()) + "," + (submittedData[j] != null ? submittedData[j] : null) + "\n";
+				schedule[j] += (member == null ? "" : member.getName()) + "," + (submittedData[j] != null ? submittedData[j] : "") + "\n";
 			}
 		}
 		return schedule;
